9bc25de915e64c8eefbfd8603f7c45b01bbc05da
bara <bayebara2000@gmail..com>
2025-07-22 00:58:31 +0000

    Ajoute public comme dossier normal

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 require_once __DIR__ . '/../config/bootstrap.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+if (file_exists(dirname(__DIR__).'/.env')) {
+    $dotenv->load();
+}
+
 try {
     $pdo = \App\Core\Database::getInstance();
     echo "Connexion à la base de données réussie.<br>";
